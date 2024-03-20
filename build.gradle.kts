129a5bd13d97fbca6f526d3f144f9ddd5c01060c
Mamoun <mamoun.imghi@epfl.ch>
2024-03-20 01:23:13 +0100

    Disabled automatic analysis and enabled CI-based

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,13 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "feedme-swent-epfl_feedme-android")
+        property("sonar.organization", "feedme-swent-epfl")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }

a7e84b5d9f1349471d63a6259da7aee241710a22
Jeanne <jeanne.decanteloubedemarmies@epfl.ch>
2024-04-10 18:14:22 +0200

    test and coverage finished

--- app/src/androidTest/java/com/android/feedme/screen/SmallThumbnailsScreen.kt
+++ app/src/androidTest/java/com/android/feedme/screen/SmallThumbnailsScreen.kt
@@ -1,14 +1,9 @@
 package com.android.feedme.screen
 
-import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onRoot
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.feedme.SmallThumbnailsDisplay
 import com.android.feedme.model.data.Ingredient
@@ -23,23 +18,21 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class SmallThumbnailsDisplayTest {
 
-    @get:Rule
-    val composeTestRule = createComposeRule()
+  @get:Rule val composeTestRule = createComposeRule()
 
-    @get:Rule
+  @Test
+  fun checkThumbnailsDisplay() {
     val recipe1 =
         Recipe(
             recipeId = "lasagna1",
             title = "Tasty Lasagna",
             description = "a",
             ingredients =
-            listOf(
-                IngredientMetaData(
-                    quantity = 2.0,
-                    measure = MeasureUnit.ML,
-                    ingredient = Ingredient("Tomato", "Vegetables", "tomatoID")
-                )
-            ),
+                listOf(
+                    IngredientMetaData(
+                        quantity = 2.0,
+                        measure = MeasureUnit.ML,
+                        ingredient = Ingredient("Tomato", "Vegetables", "tomatoID"))),
             steps = listOf(Step(1, "a", "Step1")),
             tags = listOf("Meat"),
             time = 1.15,
@@ -48,32 +41,17 @@ class SmallThumbnailsDisplayTest {
             difficulty = "Intermediate",
             "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.mamablip.com%2Fstorage%2FLasagna%2520with%2520Meat%2520and%2520Tomato%2520Sauce_3481612355355.jpg&f=1&nofb=1&ipt=8e887ba99ce20a85fb867dabbe0206c1146ebf2f13548b5653a2778e3ea18c54&ipo=images")
 
-    @Test
-    fun checkThumbnailsDisplay() {
+    composeTestRule.setContent { SmallThumbnailsDisplay(listRecipe = listOf(recipe1)) }
 
-        // Launch the activity
-        composeTestRule.setContent {
-            SmallThumbnailsDisplay(
-                listRecipe = listOf(recipe1,recipe1,recipe1,recipe1,recipe1)
-            )
-        }
+    composeTestRule.onNodeWithTag("Column").assertIsDisplayed()
 
-        // Check if the thumbnails are displayed
-        composeTestRule.onNodeWithTag("Recipe Image").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("Star Icon").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("Rating").assertTextEquals(recipe1.rating.toString())
 
-        // Check if the ratings are displayed correctly
-        composeTestRule.onNodeWithTag("Rating").assertTextEquals("4.5")
-        composeTestRule.onNodeWithTag("Rating").assertTextEquals("3.8")
-        // Add assertions for other ratings as needed
+    composeTestRule.onNodeWithTag("Info Icon").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("Time").assertTextEquals(recipe1.time.toString())
 
-        // Check if the cooking times are displayed correctly
-        composeTestRule.onNodeWithTag("Time").assertTextEquals("15")
-        composeTestRule.onNodeWithTag("Time").assertTextEquals("20")
-        // Add assertions for other cooking times as needed
-
-        // Check if the titles are displayed correctly
-        composeTestRule.onNodeWithTag("Title").assertTextEquals("Recipe 1")
-        composeTestRule.onNodeWithTag("Title").assertTextEquals("Recipe 2")
-        // Add assertions for other titles as needed
-    }
+    composeTestRule.onNodeWithTag("Save Icon").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("Recipe Title").assertTextEquals(recipe1.title)
+  }
 }
--- app/src/main/java/com/android/feedme/SmallTumbnailsDisplay.kt
+++ app/src/main/java/com/android/feedme/SmallTumbnailsDisplay.kt
@@ -10,25 +10,22 @@ import androidx.compose.material.icons.outlined.Build
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.twotone.Star
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import com.android.feedme.model.data.Ingredient
-import com.android.feedme.model.data.IngredientMetaData
-import com.android.feedme.model.data.MeasureUnit
 import com.android.feedme.model.data.Recipe
-import com.android.feedme.model.data.Step
 import com.android.feedme.ui.theme.YellowStar
-
 
 /**
  * Composable function to display a list of recipes as small thumbnails with additional information.
- * Each thumbnail includes an image, rating, cooking time, and title.
+ * Each thumbnail includes an image, rating, cooking time, and title. Thumbnails are displayed two
+ * by two in a column.
  *
  * @param listRecipe List of Recipe objects to be displayed.
  */
@@ -39,65 +36,44 @@ fun SmallThumbnailsDisplay(listRecipe: List<Recipe>) {
   LazyVerticalGrid(columns = GridCells.Adaptive(minSize = IMAGE_WIDTH.dp)) {
     items(listRecipe.size) { i ->
       Column(
-          horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(3.dp)) {
-            AsyncImage(model = listRecipe[i].imageUrl, contentDescription = "Recipe Image")
+          horizontalAlignment = Alignment.CenterHorizontally,
+          modifier = Modifier.testTag("Column").padding(3.dp)) {
+            AsyncImage(
+                model = listRecipe[i].imageUrl,
+                contentDescription = "Recipe Image",
+                modifier = Modifier.testTag("Recipe Image"))
 
             Row(verticalAlignment = Alignment.CenterVertically) {
               Icon(
                   imageVector = Icons.TwoTone.Star,
                   contentDescription = null,
                   tint = YellowStar,
-                  modifier = Modifier.padding(end = 3.dp))
+                  modifier = Modifier.testTag("Star Icon").padding(end = 3.dp))
 
-              Text(listRecipe[i].rating.toString(), modifier = Modifier.padding(end = 10.dp))
+              Text(
+                  listRecipe[i].rating.toString(),
+                  modifier = Modifier.testTag("Rating").padding(end = 10.dp))
 
               // There is no clock icon in Material, so for now i'm using the "build" icon
               Icon(
                   imageVector = Icons.Outlined.Info,
                   contentDescription = null,
-                  modifier = Modifier.padding(end = 3.dp))
+                  modifier = Modifier.testTag("Info Icon").padding(end = 3.dp))
 
-              Text(listRecipe[i].time.toString(), modifier = Modifier.padding(end = 45.dp))
+              Text(
+                  listRecipe[i].time.toString(),
+                  modifier = Modifier.testTag("Time").padding(end = 45.dp))
 
               // There is no save icon in Material, so for now i'm using the "build" icon
-              Icon(imageVector = Icons.Outlined.Build, contentDescription = null)
+              IconButton(
+                  onClick = { /*TODO call to the database function for saving recipes*/},
+                  modifier = Modifier.testTag("Save Icon")) {
+                    Icon(imageVector = Icons.Outlined.Build, contentDescription = null)
+                  }
             }
 
-            Text(text = listRecipe[i].title)
+            Text(text = listRecipe[i].title, modifier = Modifier.testTag("Recipe Title"))
           }
     }
   }
-}
-
-@Preview
-@Composable
-fun PreviewSmallThumbnailsDisplay() {
-  // fake data because we don't have a database yet
-  val recipe1 =
-      Recipe(
-          recipeId = "lasagna1",
-          title = "Tasty Lasagna",
-          description = "a",
-          ingredients =
-              listOf(
-                  IngredientMetaData(
-                      quantity = 2.0,
-                      measure = MeasureUnit.ML,
-                      ingredient = Ingredient("Tomato", "Vegetables", "tomatoID"))),
-          steps = listOf(Step(1, "a", "Step1")),
-          tags = listOf("Meat"),
-          time = 1.15,
-          rating = 4.5,
-          userid = "PasDavid",
-          difficulty = "Intermediate",
-          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.mamablip.com%2Fstorage%2FLasagna%2520with%2520Meat%2520and%2520Tomato%2520Sauce_3481612355355.jpg&f=1&nofb=1&ipt=8e887ba99ce20a85fb867dabbe0206c1146ebf2f13548b5653a2778e3ea18c54&ipo=images")
-  val testRecipes: List<Recipe> =
-      listOf(
-          recipe1,
-          recipe1,
-          recipe1,
-          recipe1,
-          recipe1,
-      )
-  SmallThumbnailsDisplay(listRecipe = testRecipes)
 }

de1199819e4b99bd4a0873ec2894942fb39813e0
Marwa Rezziki <marwa.rezziki@epfl.ch>
2024-04-11 21:33:55 +0200

    Fixing the code duplication

--- app/src/main/java/com/android/feedme/ui/LandingScreen.kt
+++ app/src/main/java/com/android/feedme/ui/LandingScreen.kt
@@ -70,57 +70,7 @@ fun LandingPage(navigationActions: NavigationActions) {
               userid = "PasDavid",
               difficulty = "Intermediate",
               "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.mamablip.com%2Fstorage%2FLasagna%2520with%2520Meat%2520and%2520Tomato%2520Sauce_3481612355355.jpg&f=1&nofb=1&ipt=8e887ba99ce20a85fb867dabbe0206c1146ebf2f13548b5653a2778e3ea18c54&ipo=images"),
-          Recipe(
-              recipeId = "lasagna1",
-              title = "Tasty Lasagna",
-              description = "je decris ma recette",
-              ingredients =
-                  listOf(
-                      IngredientMetaData(
-                          quantity = 2.0,
-                          measure = MeasureUnit.ML,
-                          ingredient = Ingredient("Tomato", "Vegetables", "tomatoID"))),
-              steps = listOf(Step(1, "a", "Step1")),
-              tags = listOf("Meat"),
-              time = 1.15,
-              rating = 4.5,
-              userid = "PasDavid",
-              difficulty = "Intermediate",
-              "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.mamablip.com%2Fstorage%2FLasagna%2520with%2520Meat%2520and%2520Tomato%2520Sauce_3481612355355.jpg&f=1&nofb=1&ipt=8e887ba99ce20a85fb867dabbe0206c1146ebf2f13548b5653a2778e3ea18c54&ipo=images"),
-          Recipe(
-              recipeId = "lasagna1",
-              title = "Tasty Lasagna",
-              description = "a",
-              ingredients =
-                  listOf(
-                      IngredientMetaData(
-                          quantity = 2.0,
-                          measure = MeasureUnit.ML,
-                          ingredient = Ingredient("Tomato", "Vegetables", "tomatoID"))),
-              steps = listOf(Step(1, "a", "Step1")),
-              tags = listOf("Meat"),
-              time = 1.15,
-              rating = 4.5,
-              userid = "PasDavid",
-              difficulty = "Intermediate",
-              "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.mamablip.com%2Fstorage%2FLasagna%2520with%2520Meat%2520and%2520Tomato%2520Sauce_3481612355355.jpg&f=1&nofb=1&ipt=8e887ba99ce20a85fb867dabbe0206c1146ebf2f13548b5653a2778e3ea18c54&ipo=images"),
-          Recipe(
-              recipeId = "lasagna1",
-              title = "Tasty Lasagna",
-              description = "a",
-              ingredients =
-                  listOf(
-                      IngredientMetaData(
-                          quantity = 2.0,
-                          measure = MeasureUnit.ML,
-                          ingredient = Ingredient("Tomato", "Vegetables", "tomatoID"))),
-              steps = listOf(Step(1, "a", "Step1")),
-              tags = listOf("Meat"),
-              time = 1.15,
-              rating = 4.5,
-              userid = "PasDavid",
-              difficulty = "Intermediate",
-              "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.mamablip.com%2Fstorage%2FLasagna%2520with%2520Meat%2520and%2520Tomato%2520Sauce_3481612355355.jpg&f=1&nofb=1&ipt=8e887ba99ce20a85fb867dabbe0206c1146ebf2f13548b5653a2778e3ea18c54&ipo=images"))
+      )
   Scaffold(
       modifier = Modifier.fillMaxSize().testTag("LandingScreen"),
       topBar = { TopBarNavigation(title = "FeedMe") },

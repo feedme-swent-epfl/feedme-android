dafefea776f1ddf4bee891b82681e749d8dfe516
Jeanne <jeanne.decanteloubedemarmies@epfl.ch>
2024-04-12 11:51:49 +0200

    UI finished

---
+++ app/src/androidTest/java/com/android/feedme/test/component/SmallCommentsTest.kt
@@ -0,0 +1,49 @@
+package com.android.feedme.test.component
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.feedme.SmallThumbnailsDisplay
+import com.android.feedme.model.data.Comment
+import com.android.feedme.model.data.Ingredient
+import com.android.feedme.model.data.IngredientMetaData
+import com.android.feedme.model.data.MeasureUnit
+import com.android.feedme.model.data.Recipe
+import com.android.feedme.model.data.Step
+import com.android.feedme.ui.navigation.SmallCommentsDisplay
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.Date
+
+@RunWith(AndroidJUnit4::class)
+class SmallCommentsTest {
+
+    @get:Rule val composeTestRule = createComposeRule()
+
+    @Test
+    fun checkCommentsDisplay() {
+        val comment1 = Comment("@author","@author","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.mamablip.com%2Fstorage%2FLasagna%2520with%2520Meat%2520and%2520Tomato%2520Sauce_3481612355355.jpg&f=1&nofb=1&ipt=8e887ba99ce20a85fb867dabbe0206c1146ebf2f13548b5653a2778e3ea18c54&ipo=images",
+            3.5,1.15,"Was uncooked","I respected instruction, but the result wasn't great", Date()
+        )
+
+        composeTestRule.setContent { SmallCommentsDisplay(listComment = listOf(comment1)) }
+
+        // Testing the presence of the star icon
+        composeTestRule.onNodeWithContentDescription("Recipe Image").assertIsDisplayed()
+
+// Testing the display of the rating text
+        composeTestRule.onNodeWithText(comment1.rating.toString()).assertIsDisplayed()
+        composeTestRule.onNodeWithText(comment1.title).assertIsDisplayed()
+
+// Waiting for the composition to become idle
+        composeTestRule.waitForIdle()
+
+// Testing the display of the comment title
+        composeTestRule.onNodeWithText(comment1.title).assertIsDisplayed()
+
+    }
+}
--- app/src/main/java/com/android/feedme/ui/navigation/SmallCommentsDisplay.kt
+++ app/src/main/java/com/android/feedme/ui/navigation/SmallCommentsDisplay.kt
@@ -1,5 +1,6 @@
 package com.android.feedme.ui.navigation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -23,14 +25,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.android.feedme.R
 import com.android.feedme.model.data.Comment
+import com.android.feedme.ui.theme.BlueUser
 import com.android.feedme.ui.theme.YellowStar
 import java.util.Date
 
@@ -38,13 +46,15 @@ import java.util.Date
 fun SmallCommentsDisplay(listComment: List<Comment>) {
   LazyColumn {
       items(listComment) {item ->
-          ItemRow(item = item)
+          //ItemRow(item = item)
+          CommentCard(comment = item)
       }
   }
 }
 
+
 @Composable
-fun ItemRow(item: Comment) {
+fun CommentCard(comment: Comment) {
     Surface(
         modifier = Modifier.fillMaxWidth(),
         color = Color.White,
@@ -55,35 +65,55 @@ fun ItemRow(item: Comment) {
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
-            // Image on the left
-            AsyncImage(
-                model = item.photoURL,
-                contentDescription = "Comment Image",
+            // Recipe image
+            Image(
+                painter = painterResource(id = R.drawable.test_image_pasta),
+                contentDescription = "Recipe Image",
                 modifier = Modifier
                     .size(100.dp)
-                    .aspectRatio(1f),
-                contentScale = ContentScale.Crop)
+                    .aspectRatio(1f)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
 
-            // Title and description on the right
+            Spacer(modifier = Modifier.width(16.dp))
+
+            // Title and description
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(start = 16.dp)
                     .align(Alignment.CenterVertically)
             ) {
-                Text(text = item.title)
+                Text(
+                    text = comment.authorId,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = BlueUser
+                )
+                //Spacer(modifier = Modifier.height(4.dp))
                 Spacer(modifier = Modifier.height(8.dp))
-                Text(text = item.content)
+                Text(
+                    text = comment.title,
+                    style = MaterialTheme.typography.titleLarge,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = comment.content,
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis
+                )
             }
         }
     }
 }
-
 @Preview
 @Composable
 fun Preview(){
-    val comment1 = Comment("author","author","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.mamablip.com%2Fstorage%2FLasagna%2520with%2520Meat%2520and%2520Tomato%2520Sauce_3481612355355.jpg&f=1&nofb=1&ipt=8e887ba99ce20a85fb867dabbe0206c1146ebf2f13548b5653a2778e3ea18c54&ipo=images",
-        3.5,1.15,"Was unccoked","I respected instruction, but the result wasn't great", Date()
+    val comment1 = Comment("@author","@author","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.mamablip.com%2Fstorage%2FLasagna%2520with%2520Meat%2520and%2520Tomato%2520Sauce_3481612355355.jpg&f=1&nofb=1&ipt=8e887ba99ce20a85fb867dabbe0206c1146ebf2f13548b5653a2778e3ea18c54&ipo=images",
+        3.5,1.15,"Was uncooked","I respected instruction, but the result wasn't great", Date()
     )
     val listComment = listOf(comment1, comment1, comment1)
     SmallCommentsDisplay(listComment = listComment)

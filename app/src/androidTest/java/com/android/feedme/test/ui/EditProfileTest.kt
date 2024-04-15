eb73bf2910fd7f9a30a4fb34f028db76e9d84b95
Florian Dejean <florian.dejean@epfl.ch>
2024-04-15 20:41:11 +0200

    Added a full mock of the Firebase for the "save" action

--- app/src/androidTest/java/com/android/feedme/test/ui/EditProfileTest.kt
+++ app/src/androidTest/java/com/android/feedme/test/ui/EditProfileTest.kt
@@ -4,12 +4,15 @@ import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.feedme.model.data.Profile
 import com.android.feedme.model.data.ProfileRepository
 import com.android.feedme.ui.navigation.NavigationActions
 import com.android.feedme.ui.profile.EditProfileScreen
+import com.google.android.gms.tasks.Tasks
 import com.google.firebase.FirebaseApp
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import io.mockk.every
 import io.mockk.mockk
@@ -29,6 +32,7 @@ class EditProfileTest {
   private val mockDocumentReference = mockk<DocumentReference>(relaxed = true)
   private val mockCollectionReference = mockk<CollectionReference>(relaxed = true)
   private var profileRepository: ProfileRepository? = null
+  private var mockDocumentSnapshot = mockk<DocumentSnapshot>(relaxed = true)
 
   @Before
   fun setupMocks() {
@@ -41,6 +45,12 @@ class EditProfileTest {
 
     every { mockFirestore.collection("profiles") } returns mockCollectionReference
     every { mockCollectionReference.document(any()) } returns mockDocumentReference
+
+    val profile = Profile()
+    every { mockDocumentReference.get() } returns Tasks.forResult(mockDocumentSnapshot)
+    every { mockDocumentSnapshot.toObject(Profile::class.java) } returns profile
+
+    every { mockDocumentReference.set(any()) } returns Tasks.forResult(null)
   }
 
   @Test
@@ -84,6 +94,6 @@ class EditProfileTest {
         .performTextInput("This is a sample bio.")
 
     // Ensure Save button is clickable and performs the expected action
-    composeTestRule.onNodeWithText("Save").assertIsEnabled().assertHasClickAction()
+    composeTestRule.onNodeWithText("Save").assertIsEnabled().assertHasClickAction().performClick()
   }
 }

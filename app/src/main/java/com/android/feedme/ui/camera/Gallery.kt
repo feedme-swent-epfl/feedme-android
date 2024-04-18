8954cf7aef81c5f924315480d4594abfa0012b3b
Mamoun <mamoun.imghi@epfl.ch>
2024-04-19 00:44:11 +0200

    Dummy screen after selection

--- app/src/main/java/com/android/feedme/ui/camera/Gallery.kt
+++ app/src/main/java/com/android/feedme/ui/camera/Gallery.kt
@@ -3,10 +3,19 @@ package com.android.feedme.ui.camera
 import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
 import androidx.core.app.ActivityCompat
-
+import com.android.feedme.ui.navigation.TopBarNavigation
 
 @Composable
 fun GalleryScreen() {
@@ -21,4 +30,14 @@ fun GalleryScreen() {
       Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
   gallery.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
   ActivityCompat.startActivityForResult(applicationContext as Activity, gallery, imageUri, null)
+  Scaffold(
+      modifier = Modifier.fillMaxSize().testTag("Gallery"),
+      topBar = { TopBarNavigation(title = "Gallery") },
+      content = { paddingValues ->
+        Box(
+            modifier = Modifier.fillMaxSize().padding(paddingValues),
+            contentAlignment = Alignment.Center) {
+              Text(text = "Not implemented yet :)")
+            }
+      })
 }

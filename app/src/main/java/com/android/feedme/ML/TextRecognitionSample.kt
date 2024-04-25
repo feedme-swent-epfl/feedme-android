81b2f6f4a2e9ecdbc6096783e775fac936022592
Jeanne <jeanne.decanteloubedemarmies@epfl.ch>
2024-04-25 22:18:33 +0200

    more testssss

--- app/src/main/java/com/android/feedme/ML/TextRecognitionSample.kt
+++ app/src/main/java/com/android/feedme/ML/TextRecognitionSample.kt
@@ -99,7 +99,7 @@ fun OverlayTextField(isVisible: Boolean, onDismiss: () -> Unit, text: String = "
           Surface(
               shape = RectangleShape,
               color = Color.White,
-              modifier = Modifier.padding(16.dp).testTag("Ml Text Box")) {
+              modifier = Modifier.padding(16.dp).testTag("ML Text Box")) {
                 Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
                   Text(text = text, modifier = Modifier.testTag("ML Text Box Inside"))
                 }

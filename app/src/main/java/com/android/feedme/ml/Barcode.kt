59afb0b9e9424f83addfffdf1eead76a7413c187
Jeanne <jeanne.decanteloubedemarmies@epfl.ch>
2024-05-02 22:05:55 +0200

    almost working !!!!

--- app/src/main/java/com/android/feedme/ml/Barcode.kt
+++ app/src/main/java/com/android/feedme/ml/Barcode.kt
@@ -2,10 +2,15 @@ package com.android.feedme.ml
 
 import android.graphics.Bitmap
 import androidx.compose.runtime.mutableStateOf
+import com.android.feedme.api.HttpMethod
+import com.android.feedme.api.httpRequestBarcode
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 /**
  * Scans a barcode from a given bitmap image using google ML kit. By default it's parametrized to
@@ -40,3 +45,43 @@ fun barcodeScanner(
     }
   }
 }
+
+@Serializable
+data class ProductData(
+    val code: String,
+    val product: String,
+    val status: Int,
+    val status_verbose: String
+)
+/**
+ * Asynchronously extracts the product name from a barcode number by doing an http request.
+ *
+ * This function performs the extraction operation asynchronously, allowing the caller to provide
+ * callbacks for both successful and failed outcomes.
+ *
+ * @param barcodeNB The barcode number for which the product name needs to be extracted.
+ * @param onSuccess Callback function invoked when the product name extraction is successful. It
+ *   takes a single parameter, [productName], representing the extracted product name.
+ * @param onFailure Callback function invoked when an exception occurs during the extraction
+ *   process. It takes a single parameter, [exception], representing the exception that occurred.
+ * @throws Exception if an error occurs during the extraction process.
+ */
+suspend fun extractProductNameFromBarcode(
+    barcodeNB: String,
+    onSuccess: (String) -> Unit,
+    onFailure: (Exception) -> Unit
+) {
+
+  try {
+    //val jsonString = httpRequestBarcode(HttpMethod.GET, barcodeNB, "fields=product_name")
+    //val productData = Json.decodeFromString<ProductData>(jsonString)
+    //onSuccess("Code : ${productData.code}, product name : ${productData.product}")
+    onSuccess(httpRequestBarcode(HttpMethod.GET, barcodeNB, "fields=product_name"))
+  } catch (e: Exception) {
+    println(e.message)
+      onFailure(e)
+
+  }
+}
+
+// Define a data class representing the JSON structure

847b706d2c38b7c7eb55ba210fd6059dc211333c
fdejean <florian.dejean@epfl.ch>
2024-05-05 17:32:42 +0200

    Finished Adding Test

--- app/src/androidTest/java/com/android/feedme/test/ui/RecipeStepInputTest.kt
+++ app/src/androidTest/java/com/android/feedme/test/ui/RecipeStepInputTest.kt
@@ -1,8 +1,11 @@
 package com.android.feedme.test.ui
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.feedme.model.data.Step
+import com.android.feedme.model.viewmodel.RecipeStepViewModel
 import com.android.feedme.screen.RecipeStepInputTestScreen
 import com.android.feedme.ui.component.StepInput
 import com.android.feedme.ui.component.StepList
@@ -14,13 +17,49 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class RecipeStepInputTest {
   @get:Rule val composeTestRule = createComposeRule()
+  lateinit var recipeStepViewModel: RecipeStepViewModel
 
   private fun launchSListInputs() {
-    composeTestRule.setContent { StepList() }
+    recipeStepViewModel = RecipeStepViewModel()
+    recipeStepViewModel.addStep(Step(1, "aled", "descc"))
+    composeTestRule.setContent {
+      StepList(Modifier.fillMaxWidth(), recipeStepViewModel = recipeStepViewModel)
+    }
   }
 
   private fun launchStepInput() {
     composeTestRule.setContent { StepInput(Step(1, "aled", "descc"), {}, {}) }
+  }
+
+  @Test
+  fun fullTest() {
+    launchSListInputs()
+    ComposeScreen.onComposeScreen<RecipeStepInputTestScreen>(composeTestRule) {
+      titleList.assertIsDisplayed()
+      expandButton.assertIsDisplayed()
+
+      deleteButton.assertIsDisplayed()
+      deleteButton.assertHasClickAction()
+      deleteButton.performClick()
+      assert(recipeStepViewModel.steps.value.isEmpty())
+
+      addStepButton.assertIsDisplayed()
+      addStepButton.assertHasClickAction()
+      addStepButton.performClick()
+      assert(recipeStepViewModel.steps.value.size == 1)
+
+      titleField.performClick()
+      titleField.performTextInput("Sample Title")
+
+      descriptionField.performClick()
+      descriptionField.performTextInput("Sample Description")
+      assert(recipeStepViewModel.steps.value.first().stepNumber == 1)
+      assert(recipeStepViewModel.steps.value.first().title == "Sample Title")
+      assert(recipeStepViewModel.steps.value.first().description == "Sample Description")
+
+      addStepButton.performClick()
+      assert(recipeStepViewModel.steps.value.size == 2)
+    }
   }
 
   @Test
@@ -50,18 +89,22 @@ class RecipeStepInputTest {
 
   @Test
   fun testDescriptionField_ExpandAndValidate() {
-    launchStepInput()
+    launchSListInputs()
     ComposeScreen.onComposeScreen<RecipeStepInputTestScreen>(composeTestRule) {
+      expandButton.assertIsDisplayed()
+
       expandButton.performClick() // Expand to show the description field
+      descriptionField.assertIsDisplayed()
+      descriptionField.performClick()
       descriptionField.performTextClearance()
-      descriptionField.performTextInput("Sample Description")
-      composeTestRule.waitForIdle()
-      descriptionField.performTextClearance()
-      descriptionField.performTextInput("")
-      composeTestRule.waitForIdle()
 
       descriptionError.assertIsDisplayed()
       descriptionError.assertTextEquals("Description cannot be empty.")
+
+      descriptionField.performClick()
+      descriptionField.performTextInput("Sample Description")
+
+      composeTestRule.waitForIdle()
     }
   }
 

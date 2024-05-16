c9023c5ac3e52f9ac9ecf6375be2fdbed21ef6ac
ursulael-khoury <ursulakhoury2003@hotmail.com>
2024-05-16 11:56:40 +0200

    ktfmt formatting fix

--- app/src/main/java/com/android/feedme/ui/home/LandingScreen.kt
+++ app/src/main/java/com/android/feedme/ui/home/LandingScreen.kt
@@ -254,11 +254,12 @@ fun RecipeCard(
                               },
                           contentDescription = "Bookmark Icon on Recipe Card",
                           modifier = Modifier.size(34.dp).padding(start = 4.dp),
-                          tint = if (isSaved.value) {
-                              YellowStar
-                          } else {
-                              YellowStarBlackOutline
-                          })
+                          tint =
+                              if (isSaved.value) {
+                                YellowStar
+                              } else {
+                                YellowStarBlackOutline
+                              })
                     }
               }
               Row(verticalAlignment = Alignment.CenterVertically) {

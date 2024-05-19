48e8961836f2065b8de6c70ac30d1d8eba378fbe
Jeanne <jeanne.decanteloubedemarmies@epfl.ch>
2024-05-19 18:25:26 +0200

    Removed an import

--- app/src/main/java/com/android/feedme/ui/component/IngredientList.kt
+++ app/src/main/java/com/android/feedme/ui/component/IngredientList.kt
@@ -35,7 +35,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.PopupProperties
 import com.android.feedme.model.data.Ingredient

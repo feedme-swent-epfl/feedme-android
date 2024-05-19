8b697044f3077b0235fb0af0e3aee15b78ef4cc9
ohamz <omar.hammoudx@gmail.com>
2024-05-19 19:11:45 +0200

    fixed width and kftmt

--- app/src/main/java/com/android/feedme/ui/navigation/TopBarNavigation.kt
+++ app/src/main/java/com/android/feedme/ui/navigation/TopBarNavigation.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
@@ -72,8 +73,9 @@ fun TopBarNavigation(
                     }
                   }
 
+              // Title
               Text(
-                  modifier = Modifier.testTag("TitleText"),
+                  modifier = Modifier.testTag("TitleText").width(250.dp),
                   text = title,
                   fontSize = 20.sp,
                   fontWeight = FontWeight.Bold,

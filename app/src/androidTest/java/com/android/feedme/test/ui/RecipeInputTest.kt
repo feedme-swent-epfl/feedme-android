b317fc865f91b9da1bada6f5d87be03a65bba266
ohamz <omar.hammoudx@gmail.com>
2024-05-19 22:16:24 +0200

    UI test works

--- app/src/androidTest/java/com/android/feedme/test/ui/RecipeInputTest.kt
+++ app/src/androidTest/java/com/android/feedme/test/ui/RecipeInputTest.kt
@@ -2,11 +2,16 @@ package com.android.feedme.test.ui
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.feedme.model.data.IngredientsRepository
 import com.android.feedme.model.data.RecipeRepository
 import com.android.feedme.model.viewmodel.ProfileViewModel
+import com.android.feedme.screen.RecipeInputTestScreen
 import com.android.feedme.ui.navigation.NavigationActions
+import com.android.feedme.ui.profile.RecipeInputScreen
 import com.google.firebase.firestore.FirebaseFirestore
+import io.github.kakaocup.compose.node.element.ComposeScreen
 import io.mockk.mockk
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,50 +26,47 @@ class RecipeInputTest {
   private val profileViewModel: ProfileViewModel = mockk(relaxed = true)
 
   private lateinit var recipeRepository: RecipeRepository
+  private lateinit var ingredientsRepository: IngredientsRepository
 
-  //  @Before
-  //  fun setUp() {
-  //    RecipeRepository.initialize(mockFirestore)
-  //    recipeRepository = RecipeRepository.instance
-  //  }
+  @Before
+  fun setUp() {
+    RecipeRepository.initialize(mockFirestore)
+    IngredientsRepository.initialize(mockFirestore)
+
+    recipeRepository = RecipeRepository.instance
+    ingredientsRepository = IngredientsRepository.instance
+  }
 
   @Test
-  fun validate() {
-    assert(true)
+  fun recipeInputTestDisplayedAndValidates() {
+    composeTestRule.setContent { RecipeInputScreen(navigationActions, profileViewModel) }
+
+    composeTestRule.waitForIdle()
+
+    ComposeScreen.onComposeScreen<RecipeInputTestScreen>(composeTestRule) {
+      topBar { assertIsDisplayed() }
+      bottomBar { assertIsDisplayed() }
+      recipeInputBox { assertIsDisplayed() }
+      recipePicture { assertIsDisplayed() }
+
+      validateRecipe {
+        assertIsDisplayed()
+        assertHasClickAction()
+      }
+
+      titleInput {
+        assertIsDisplayed()
+        // TODO add input
+      }
+      ingredientsInput { assertIsDisplayed() }
+      // stepsInput { assertIsDisplayed() }
+
+      validateRecipe {
+        assertIsDisplayed()
+        assertHasClickAction()
+      }
+    }
   }
-  //  @Test
-  //  fun recipeInputTestDisplayedAndValidates() {
-  //    composeTestRule.setContent {
-  //      RecipeInputScreen(
-  //        mockk<NavigationActions>(relaxed = true), mockk<ProfileViewModel>(relaxed = true))
-  //    }
-  //
-  //    composeTestRule.waitForIdle()
-  //
-  //    ComposeScreen.onComposeScreen<RecipeInputTestScreen>(composeTestRule) {
-  //      topBar { assertIsDisplayed() }
-  //      bottomBar { assertIsDisplayed() }
-  //      recipeInputBox { assertIsDisplayed() }
-  //      recipePicture { assertIsDisplayed() }
-  //
-  //      validateRecipe {
-  //        assertIsDisplayed()
-  //        assertHasClickAction()
-  //      }
-  //
-  //      titleInput {
-  //        assertIsDisplayed()
-  //        // TODO add input
-  //      }
-  //      ingredientsInput { assertIsDisplayed() }
-  //      stepsInput { assertIsDisplayed() }
-  //
-  //      validateRecipe {
-  //        assertIsDisplayed()
-  //        assertHasClickAction()
-  //      }
-  //    }
-  //  }
 
   //  @Test
   //  fun recipeViewModelValidateFails() {

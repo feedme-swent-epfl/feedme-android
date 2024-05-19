4a2363020349e14ebe774b6933d30bf3d3dba979
FacerOfGod <spichot2@gmail.com>
2024-05-19 19:19:17 +0200

    Merge : merge fix

--- app/src/androidTest/java/com/android/feedme/test/component/IngredientListTest.kt
+++ app/src/androidTest/java/com/android/feedme/test/component/IngredientListTest.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4

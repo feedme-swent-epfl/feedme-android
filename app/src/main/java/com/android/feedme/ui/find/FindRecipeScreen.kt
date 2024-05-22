77615845c940888cdb088d85b695bababec0bf72
ursulael-khoury <ursulakhoury2003@hotmail.com>
2024-05-22 21:49:26 +0200

    Popup implemented

--- app/src/main/java/com/android/feedme/ui/find/FindRecipeScreen.kt
+++ app/src/main/java/com/android/feedme/ui/find/FindRecipeScreen.kt
@@ -2,31 +2,44 @@ package com.android.feedme.ui.find
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.android.feedme.R
 import com.android.feedme.model.viewmodel.InputViewModel
 import com.android.feedme.ui.component.IngredientList
@@ -46,6 +59,76 @@ import com.android.feedme.ui.theme.TextBarColor
  */
 @Composable
 fun FindRecipeScreen(navigationActions: NavigationActions, inputViewModel: InputViewModel) {
+
+    val showDialog = remember { mutableStateOf(false) }
+
+    if (showDialog.value) {
+        Dialog(onDismissRequest = { showDialog.value = false }) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                color = MaterialTheme.colorScheme.background
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Info,
+                        contentDescription = "Information Icon",
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.size(40.dp).padding(end = 8.dp)
+
+                    )
+                    Text(modifier = Modifier.fillMaxWidth(), text = "Please validate how you want to generate the recipes.", textAlign = TextAlign.Center)
+                    Text(modifier = Modifier.fillMaxWidth(), text = "If you choose strict, the recipe will only include the ingredients you have chosen to input.", textAlign = TextAlign.Center)
+                    Text(text = "If you choose extra, the recipe will include the ingredients you have inputted and may include additional ingredients.",
+                        modifier = Modifier.padding(bottom = 16.dp).fillMaxWidth(),
+                        textAlign = TextAlign.Center)
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Button(
+                            onClick = { /* Handle strict ingredients validation */ },
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            Text(text = "Strict",
+                                style = TextStyle(
+                                    color = TextBarColor,
+                                    fontSize = 18.sp,
+                                ))
+                        }
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Button(
+                            onClick = { /* Handle extra ingredients validation */ },
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            Text(text = "Extra",
+                                style = TextStyle(
+                                    color = TextBarColor,
+                                    fontSize = 18.sp,
+                                ))
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "Cancel",
+                        style = TextStyle(
+                            color = Color.Red,
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Bold,
+                            textDecoration = TextDecoration.Underline
+                        ),
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .clickable { showDialog.value = false }
+                    )
+                }
+            }
+        }
+    }
 
   Scaffold(
       modifier = Modifier.testTag("FindRecipeScreen"),
@@ -69,7 +152,7 @@ fun FindRecipeScreen(navigationActions: NavigationActions, inputViewModel: Input
           FloatingActionButton(
               containerColor = FabColor,
               contentColor = TextBarColor,
-              onClick = { /*TODO Validate the ingredients*/},
+              onClick = { showDialog.value = true },
               content = {
                 Icon(
                     imageVector = Icons.Default.Check,

f203d89b7d7bb89899a2e9549b517b7c9f24b98f
ursulael-khoury <ursulakhoury2003@hotmail.com>
2024-06-02 18:28:02 +0200

    fix sonarcloud issue that was added

--- app/src/main/java/com/android/feedme/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/android/feedme/ui/profile/ProfileScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FloatingActionButton
